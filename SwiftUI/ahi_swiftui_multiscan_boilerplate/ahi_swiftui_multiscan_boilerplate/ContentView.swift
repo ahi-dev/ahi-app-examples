0fcc359292dcaa3acefdde374a05ad1fcac35331
Sabrina Hoque Tuli <sabrina.tuli07@gmail.com>
2022-04-05 11:23:56 +0800

    create swiftui bp project

---
+++ SwiftUI/ahi_swiftui_multiscan_boilerplate/ahi_swiftui_multiscan_boilerplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ahi_swiftui_multiscan_boilerplate
+//
+//  Created by Sabrina Tuli on 5/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI/ahi_swiftui_multiscan_boilerplate/ahi_swiftui_multiscan_boilerplate/ahi_swiftui_multiscan_boilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ahi_swiftui_multiscan_boilerplateApp.swift
+//  ahi_swiftui_multiscan_boilerplate
+//
+//  Created by Sabrina Tuli on 5/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct ahi_swiftui_multiscan_boilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
