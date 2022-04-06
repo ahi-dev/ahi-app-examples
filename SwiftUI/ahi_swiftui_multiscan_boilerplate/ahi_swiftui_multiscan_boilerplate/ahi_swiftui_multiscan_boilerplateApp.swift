928d8e94a28023e5595727d9306cf62838572e93
Sabrina Hoque Tuli <sabrina.tuli07@gmail.com>
2022-04-06 15:20:19 +0800

    complete boilerplate

--- SwiftUI/ahi_swiftui_multiscan_boilerplate/ahi_swiftui_multiscan_boilerplate/ahi_swiftui_multiscan_boilerplateApp.swift
+++ SwiftUI/ahi_swiftui_multiscan_boilerplate/ahi_swiftui_multiscan_boilerplate/ahi_swiftui_multiscan_boilerplateApp.swift
@@ -1,9 +1,19 @@
 //
-//  ahi_swiftui_multiscan_boilerplateApp.swift
-//  ahi_swiftui_multiscan_boilerplate
+//  AHI - Example Code
 //
-//  Created by Sabrina Tuli on 6/4/2022.
+//  Copyright (c) Advanced Human Imaging. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 
 import SwiftUI
 
