4c26ce049998fb5ead8c0b05e5708dcbe2749f3a
Potti <louisa0103a@gmail.com>
2022-04-07 14:49:13 +0800

    update gradle and lines break change

--- Kotlin/ahi_kotlin_multiscan_boilerplate/app/src/androidTest/java/com/example/ahi_kotlin_multiscan_boilerplate/ExampleInstrumentedTest.kt
+++ Kotlin/ahi_kotlin_multiscan_boilerplate/app/src/androidTest/java/com/example/ahi_kotlin_multiscan_boilerplate/ExampleInstrumentedTest.kt
@@ -14,6 +14,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+
 package com.example.ahi_kotlin_multiscan_boilerplate
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
