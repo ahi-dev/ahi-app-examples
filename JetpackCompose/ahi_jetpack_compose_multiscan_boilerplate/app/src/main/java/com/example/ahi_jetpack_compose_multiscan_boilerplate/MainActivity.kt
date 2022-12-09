21a977be5a00cfaccb54757a81defdc626c1d668
Potti <louisa0103a@gmail.com>
2022-12-09 11:25:12 +0800

    fixed download issue + decupling areResourcesDownloaded and checkResourcesdownloadSize

--- JetpackCompose/ahi_jetpack_compose_multiscan_boilerplate/app/src/main/java/com/example/ahi_jetpack_compose_multiscan_boilerplate/MainActivity.kt
+++ JetpackCompose/ahi_jetpack_compose_multiscan_boilerplate/app/src/main/java/com/example/ahi_jetpack_compose_multiscan_boilerplate/MainActivity.kt
@@ -41,6 +41,7 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.advancedhumanimaging.sdk.bodyscan.BodyScan
 import com.advancedhumanimaging.sdk.bodyscan.common.BodyScanError
+import com.advancedhumanimaging.sdk.common.IAHIDownloadProgress
 import com.advancedhumanimaging.sdk.common.IAHIPersistence
 import com.advancedhumanimaging.sdk.common.IAHIScan
 import com.advancedhumanimaging.sdk.common.models.AHIResult
@@ -76,7 +77,7 @@ class MainActivity : ComponentActivity() {
         val buttonHeight = 55.dp
         checkPermission()
         @Composable
-        fun defaultButton(buttonText: String, isEnabled: Boolean?, action: ()-> Unit) {
+        fun defaultButton(buttonText: String, isEnabled: Boolean?, action: () -> Unit) {
             Button(
                 colors = buttonColors(backgroundColor = Color.Black),
                 modifier = Modifier.height(buttonHeight),
@@ -198,9 +199,9 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun didTapDownloadResources() {
+        checkAHIResourcesDownloadSize()
         downloadAHIResources()
         areAHIResourcesAvailable()
-        checkAHIResourcesDownloadSize()
     }
 
     /**
@@ -249,11 +250,6 @@ class MainActivity : ComponentActivity() {
                     Log.d(TAG, "AHI: Resources ready\n")
                 } else {
                     Log.d(TAG, "AHI INFO: Resources are not downloaded\n")
-                    GlobalScope.launch {
-                        delay(30000)
-                        checkAHIResourcesDownloadSize()
-                        areAHIResourcesAvailable()
-                    }
                 }
             }, {
                 Log.d(TAG, "AHI: Error in resource downloading \n")
@@ -266,17 +262,31 @@ class MainActivity : ComponentActivity() {
      *  We recommend only calling this function once per session to prevent duplicate background resource calls.
      */
     private fun downloadAHIResources() {
-        AHIMultiScan.downloadResourcesInForeground()
+        AHIMultiScan.downloadResourcesInForeground(3)
     }
 
     /** Check the size of the AHI resources that require downloading. */
     private fun checkAHIResourcesDownloadSize() {
-        AHIMultiScan.totalEstimatedDownloadSizeInBytes {
-            it.fold({
-                Log.d(TAG, "AHI INFO: Size of download is ${it.progressBytes / 1024 / 1024} / ${it.totalBytes / 1024 / 1024}\n")
-            }, {
-                Log.e(TAG, it.message.toString())
-            })
+        AHIMultiScan.delegateDownloadProgress = object : IAHIDownloadProgress {
+            override fun downloadProgressReport(status: AHIResult<Unit>) {
+                if(status.isFailure){
+                    Log.d(TAG, "AHI: Failed to download resources: ${status.error().toString()}")
+                }else {
+                    AHIMultiScan.totalEstimatedDownloadSizeInBytes {
+                        it.fold({downloadState->
+                            val mB = 1024.0 * 1024.0
+                            val progress = downloadState.progressBytes / mB
+                            val total = downloadState.totalBytes / mB
+                            if (progress == total) {
+                                viewModel.isFinishedDownloadingResourcesState.value = true
+                            }
+                            Log.d(TAG, "AHI INFO: Size of download is ${progress.format(2)}MB / ${total.format(2)}MB")
+                        }, {
+                            Log.e(TAG, it.message.toString())
+                        })
+                    }
+                }
+            }
         }
     }
 
@@ -388,7 +398,7 @@ class MainActivity : ComponentActivity() {
      *  The 3D mesh can be created and returned at any time.
      *  We recommend doing this on successful completion of a body scan with the results.
      */
-    private  fun getBodyScanExtras(result: Map<String, Any>) {
+    private fun getBodyScanExtras(result: Map<String, Any>) {
         val options = mapOf("extrapolate" to listOf("mesh"))
         AHIMultiScan.getScanExtra(result, options, completionBlock = {
             it.fold({
@@ -513,8 +523,8 @@ class MainActivity : ComponentActivity() {
      * */
     private fun areSharedScanConfigOptionsValid(avatarValues: java.util.HashMap<String, Any>): Boolean {
         val sex = avatarValues["enum_ent_sex"].takeIf { it is String }
-        val height = avatarValues["cm_ent_height"].takeIf { it is Double || it is Int}
-        val weight = avatarValues["kg_ent_weight"].takeIf { it is Double || it is Int}
+        val height = avatarValues["cm_ent_height"].takeIf { it is Double || it is Int }
+        val weight = avatarValues["kg_ent_weight"].takeIf { it is Double || it is Int }
         return if (sex != null && height != null && weight != null) {
             arrayListOf("male", "female").contains(sex)
         } else {
@@ -569,7 +579,7 @@ class MainActivity : ComponentActivity() {
         return (scanLength != null &&
                 instruction1 != null &&
                 instruction2 != null &&
-                scanLength >= 20 )
+                scanLength >= 20)
     }
 
     /**
@@ -598,7 +608,7 @@ class MainActivity : ComponentActivity() {
     private fun checkPermission() {
         if (ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA)
             == PackageManager.PERMISSION_DENIED
-        ){
+        ) {
             requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CODE)
         }
     }
@@ -613,4 +623,5 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    private fun Double.format(digits: Int) = "%.${digits}f".format(this)
 }
